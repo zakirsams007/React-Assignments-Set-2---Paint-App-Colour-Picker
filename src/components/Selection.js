2f42dea096d173b2234683f350e568a6d7d69e5d
zakirsams <zakirsams007@gmail.com>
2021-05-24 21:00:39 +0530

    push

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, {useState} from 'react';
 
+
+const Selection = (props) =>{
+  let [component, updateSelectionStyle] = useState({background:''});
+  const {applyColor} = props;
+  return(
+    <div className="fix-box" onClick={()=>applyColor(updateSelectionStyle)} style={{background: component.background}}>
+    <h2 className="subheadingS">Selection</h2>
+    </div>
+  );
+}
+export default Selection;
